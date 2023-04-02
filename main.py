aa47ec1dddd68d086073d42df12c962b0361d202
arsennevestiuk <128983691+arsennevestiuk@users.noreply.github.com>
2023-04-02 19:00:26 +0300

    u7j6t
    rh7th

---
+++ main.py
@@ -0,0 +1,17 @@
+import PyGame
+
+window = display.set_mode((700,500))
+
+game = True
+clock = time.Clock()
+
+while game:
+    for e in event.get():
+        if e.type ==QUIT:
+            game = False
+
+
+    window.fill((10,100,100))
+
+    display.update()
+    clock.tick(60)
