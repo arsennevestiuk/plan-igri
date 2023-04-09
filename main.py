bbd4fa2cc74035005f91ceb0897dedb81fa93160
bogdan sherba <128983691+arsennevestiuk@users.noreply.github.com>
2023-04-09 19:05:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from pygame import *
+
 from button import Button
 
-window = display.set_mode((700,800))
-FPS = 60
+
+window = display.set_mode((700,500))
+
 
 game = True
+clock = time.Clock()
 
-btn1 = Button("start_btn.png", 100,100,100,50)
+btn1 = Button('start_btn.png', 100,100, 100, 50)
 
 
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
 
 
-display.update()
-clock.tick(FPS)
+    window.fill((200,10,20))
+
+    if btn1.draw(window):
+        print("PRESSSED")
+    display.update()
+    clock.tick(60)
