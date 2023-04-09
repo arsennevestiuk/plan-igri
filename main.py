4eeb158606d2c53cdd0ebaa371772f98a05d9c94
arsennevestiuk <128983691+arsennevestiuk@users.noreply.github.com>
2023-04-09 18:32:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from pygame import *
+from button import Button
 
+window = display.set_mode((700,800))
+FPS = 60
+
+game = True
+
+btn1 = Button("start_btn.png", 100,100,100,50)
+
+
+
+
+display.update()
+clock.tick(FPS)
